2b90b3c0eacc06096f132a70b7bd48d0af9dfc41
Xotic750 <xotic750@gmail.com>
2017-07-02 14:55:15 +0200

    Update bolierplate and dependencies

--- index.js
+++ index.js
@@ -28,52 +28,46 @@
  *
  * @see {@link http://www.ecma-international.org/ecma-262/6.0/#sec-number.isfinite|20.1.2.2 Number.isFinite ( number )}
  *
- * @version 1.1.0
+ * @version 1.2.0
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
  * @module is-finite-x
  */
 
-/* eslint strict: 1 */
+'use strict';
 
-/* global module */
+var $isNaN = require('is-nan');
 
-;(function () { // eslint-disable-line no-extra-semi
+var $isFinite;
+if (typeof Number.isFinite === 'function') {
+  var MAX_SAFE_INTEGER = require('max-safe-integer');
+  try {
+    if (Number.isFinite(MAX_SAFE_INTEGER) && Number.isFinite(Infinity) === false) {
+      $isFinite = Number.isFinite;
+    }
+  } catch (ignore) {}
+}
 
-  'use strict';
-
-  var $isNaN = require('is-nan');
-
-  var $isFinite;
-  if (typeof Number.isFinite === 'function') {
-    try {
-      if (Number.isFinite(9007199254740991) && !Number.isFinite(Infinity)) {
-        $isFinite = Number.isFinite;
-      }
-    } catch (ignore) {}
-  }
-
-  /**
-   * This method determines whether the passed value is a finite number.
-   *
-   * @param {*} number The value to be tested for finiteness.
-   * @return {boolean} A Boolean indicating whether or not the given value is a finite number.
-   * @example
-   * var numIsFinite = require('is-finite-x');
-   *
-   * numIsFinite(Infinity);  // false
-   * numIsFinite(NaN);       // false
-   * numIsFinite(-Infinity); // false
-   *
-   * numIsFinite(0);         // true
-   * numIsFinite(2e64);      // true
-   *
-   * numIsFinite('0');       // false, would've been true with
-   *                         // global isFinite('0')
-   * numIsFinite(null);      // false, would've been true with
-   */
-  module.exports = $isFinite || function isFinite(number) {
-    return !(typeof number !== 'number' || $isNaN(number) || number === Infinity || number === -Infinity);
-  };
-}());
+/**
+ * This method determines whether the passed value is a finite number.
+ *
+ * @param {*} number - The value to be tested for finiteness.
+ * @returns {boolean} A Boolean indicating whether or not the given value is a finite number.
+ * @example
+ * var numIsFinite = require('is-finite-x');
+ *
+ * numIsFinite(Infinity);  // false
+ * numIsFinite(NaN);       // false
+ * numIsFinite(-Infinity); // false
+ *
+ * numIsFinite(0);         // true
+ * numIsFinite(2e64);      // true
+ *
+ * numIsFinite('0');       // false, would've been true with
+ *                         // global isFinite('0')
+ * numIsFinite(null);      // false, would've been true with
+ */
+module.exports = $isFinite || function isFinite(number) {
+  return !(typeof number !== 'number' || $isNaN(number) || number === Infinity || number === -Infinity);
+};
