b2144ed3d0863f8f1bcddea38abb76dc8905900b
Graham Fairweather <xotic750@gmail.com>
2019-07-21 23:54:48 +0200

    :bookmark: v4.0.16
    Auto build

--- dist/is-finite-x.js
+++ dist/is-finite-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-19T21:32:49.743Z",
+  "date": "2019-07-21T21:54:43.091Z",
   "describe": "",
   "description": "ES6-compliant shim for Number.isFinite.",
   "file": "is-finite-x.js",
   "hash": "dd01d098f3280e358076",
   "license": "MIT",
-  "version": "4.0.15"
+  "version": "4.0.16"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
