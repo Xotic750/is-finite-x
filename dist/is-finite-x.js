d2d54270f628ac9cc821e152f5f2ef1732b08f1c
Graham Fairweather <xotic750@gmail.com>
2019-08-27 21:48:06 +0200

    Maintenance

--- dist/is-finite-x.js
+++ dist/is-finite-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-08-20T12:52:42.925Z",
+  "date": "2019-08-27T19:47:59.930Z",
   "describe": "",
   "description": "ES6-compliant shim for Number.isFinite.",
   "file": "is-finite-x.js",
